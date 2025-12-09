9f040602db5e48269ff80549032e9bd601f0533d
didihtml <diogolindo5448@gmail.com>
2025-12-08 23:02:33 -0300

    adjust container width

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-function toggleMode(){
-const html= document.documentElement
-html.classList.toggle("light")
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
+  const img = document.querySelector("#profile img");
 
-const img=document.querySelector("#profile img")
-
-
-
-if(html.classList.contains('light')){
-img.setAttribute('src', './assents/avatar-light.png')
-}else{
-img.setAttribute('src','./assents/avatar.png')
-}
-
-//pegar a tag img
-//se tiver light mode,adicionar a imagem ligh
-//se tiver sem light mode,manter a imagem normal
-//substituir a image
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assents/avatar-light.png");
+  } else {
+    img.setAttribute("src", "./assents/avatar.png");
+  }
 }
